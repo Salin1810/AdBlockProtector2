71317bd6fc6104c0f5975894c95251603f270b94
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-11-16 16:51:39 -0700

    No commit message

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,8 +4,10 @@
  * Remote configuration and assets URL.
  * @const {string}
  */
-const REMOTE_CONFIG = "";
-const REMOTE_ASSETS = "";
+const REMOTE_CONFIG
+    ;
+const REMOTE_ASSETS
+    ;
 
 (async () => {
     const chrome = window.chrome || window.browser;
@@ -15,41 +17,81 @@ const REMOTE_ASSETS = "";
         return;
     }
 
+    //Micro.debug = true;
+
+    let settings;
+    loadSetting: {
+        try {
+            await new Promise((resolve, reject) => {
+                chrome.storage.local.get(["config_last_update", "assets_last_update"], (items) => {
+                    if (chrome.runtime.lastError) {
+                        reject(chrome.runtime.lastError);
+                    } else {
+                        settings.config_last_update = items.config_last_update || 0;
+                        settings.assets_last_update = items.assets_last_update || 0;
+                        resolve();
+                    }
+                });
+            });
+        } catch (err) {
+            console.error("AdBlock Protector 2 could not to read savedata!");
+            return;
+        }
+    }
+
     initMicro: {
         await Micro.init();
 
-        if (Micro.config.length === 0) {
+        if (Micro.config.length === 0 || settings.config_last_update + 86400000 < Date.now()) {
             let filter;
             try {
+                if (!REMOTE_CONFIG) {
+                    throw new Error("No remote filters");
+                }
+
                 filter = await fetch(REMOTE_CONFIG);
                 filter = filter.text();
             } catch (err) {
-                console.error("AdBlock Protector 2 failed to load configuration!");
+                console.error("AdBlock Protector 2 failed to update configuration!");
+
+                filter = await fetch(chrome.runtime.getURL("config.txt"));
+                filter = filter.text();
             }
 
             if (filter) {
+                // No need to update other stuff
+                chrome.storage.local.set({ config_last_update: Date.now() });
+
                 await Micro.setConfig(filter);
                 await Micro.init();
             }
         }
 
-        if (Micro.assets.length === 0) {
+        if (Micro.assets.length === 0 || settings.assets_last_update + 86400000 < Date.now()) {
             let assets;
 
-            // Quantum extension store does not allow remote script
-            if (/firefox/i.test(navigator.userAgent)) {
+            try {
+                if (!REMOTE_ASSETS) {
+                    throw new Error("No remote assets");
+                }
+
+                // Quantum extension store does not allow remote script
+                if (/firefox/i.test(navigator.userAgent)) {
+                    throw new Error("Remote script disallowed");
+                }
+
+                assets = await fetch(REMOTE_ASSETS);
+                assets = assets.text();
+            } catch (err) {
+                console.error("AdBlock Protector 2 failed to update assets!");
+
                 assets = await fetch(chrome.runtime.getURL("assets.txt"));
                 assets = assets.text();
-            } else {
-                try {
-                    assets = await fetch(REMOTE_ASSETS);
-                    assets = assets.text();
-                } catch (err) {
-                    console.error("AdBlock Protector 2 failed to load assets!");
-                }
             }
 
             if (assets) {
+                chrome.storage.local.set({ assets_last_update: Date.now() });
+
                 await Micro.setAssets(assets);
                 await Micro.init();
             }
@@ -66,6 +108,28 @@ const REMOTE_ASSETS = "";
             if (/ublock origin/i.test(extension.name)) {
                 Micro.teardown();
                 return true;
+            }
+        });
+
+        chrome.management.onInstalled.addListener((info) => {
+            if (/ublock origin/i.test(info.name)) {
+                Micro.teardown();
+            }
+        });
+        chrome.management.onUninstalled.addListener((info) => {
+            if (/ublock origin/i.test(info.name)) {
+                Micro.init();
+            }
+        });
+
+        chrome.management.onEnabled.addListener((info) => {
+            if (/ublock origin/i.test(info.name)) {
+                Micro.teardown();
+            }
+        });
+        chrome.management.onDisabled.addListener((info) => {
+            if (/ublock origin/i.test(info.name)) {
+                Micro.init();
             }
         });
     }
